b3ff0592fcccca5fbc22ac777f6cb072a5129144
edudzikorku <kedudzi007@gmail.com>
2023-04-06 01:25:24 +0000

    Bump version: 0.0.1 → 0.0.2

--- palm_kernel/__init__.py
+++ palm_kernel/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Edudzi Akpakli"""
 __email__ = 'kedudzi007@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/edudzikorku/palm-kernel',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
